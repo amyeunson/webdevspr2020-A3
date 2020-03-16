44a75bf23a94b12aa3d2ffcf32b16094ce991132
aaeunson <eunson.a@husky.neu.edu>
2020-03-16 16:10:53 -0700

    create react app with dependencies: bootstrap, reactstrap, redux

---
+++ README.old.md
@@ -0,0 +1,1 @@
+"# webdevspr2020_a3" 
---
+++ src/views/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <p>Put your components here</p>
+    )
+  }
+}
+
+export default App;
