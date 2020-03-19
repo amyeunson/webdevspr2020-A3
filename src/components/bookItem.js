fa168eb38b0c6ea23b89c3a22aee0d2ad870897e
Leah Mamaril <8636027+Lmamaril@users.noreply.github.com>
2020-03-19 13:17:14 -0700

    Added ability to render two book lists

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -1,11 +1,16 @@
 
 import React, { Component } from 'react';
-
+import {Container, Button} from 'reactstrap';
+ 
 class BookItem extends Component {
   render() {
     return (
-      <p>Put your components here</p>
-      //create a bookItem with props
+      <div>
+              <h6>{this.props.author}</h6>
+              <Button outline color="primary" className="mt2" size="sm" >Swap</Button>
+              <Button outline color="danger" className="mt2" size="sm">Delete</Button>
+          
+      </div>
     )
   }
 }
--- src/components/lists.js
+++ src/components/lists.js
@@ -1,13 +1,58 @@
 
 import React, { Component } from 'react';
+import BookItem from './bookItem';
+import { connect } from 'react-redux';
+import {Container, Card, CardTitle, CardHeader, CardBody, CardText} from 'reactstrap';
 
 class Lists extends Component {
   render() {
     return (
-      <p>Put your components here</p>
-      // For each item in List, create a BookItem
+      <div>
+      {/* To Read List  */}
+      <Container>
+        <Card >
+          <CardHeader>To Read List</CardHeader>
+          {/* Book Item  */}
+          <CardBody>
+              {this.props.toReadList.map((book)=>
+                <div>
+                  <CardTitle>{book.title}</CardTitle>
+                    <CardText>  
+                      <BookItem className="mt-2"author={book.author}/>
+                    </CardText>
+                </div>     
+              )}      
+          </CardBody>
+        </Card>
+      </Container>
+      
+      {/* Have Read List  */}
+      <Container>
+        <Card >
+          <CardHeader>Have Read List</CardHeader>
+          {/* Book Item  */}
+          <CardBody>
+              {this.props.haveReadList.map((book)=>
+                <div>
+                  <CardTitle>{book.title}</CardTitle>
+                    <CardText>  
+                      <BookItem author={book.author}/>
+                    </CardText>
+                </div>     
+              )}      
+          </CardBody>
+        </Card>
+      </Container>
+    </div>
     )
   }
 }
 
-export default Lists;
+let mapStateToProps = function mapStateToProps(state, props) {
+  return { 
+    toReadList: state.bookLists.toRead,
+    haveReadList: state.bookLists.haveRead
+  }
+}
+
+export default connect(mapStateToProps)(Lists);
