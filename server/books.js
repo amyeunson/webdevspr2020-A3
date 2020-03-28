e4a9d4c01c4c6de77e2b4a4dccd160d66be72772
Leah Mamaril <8636027+Lmamaril@users.noreply.github.com>
2020-03-27 22:48:53 -0700

    Remove SearchList from ServerSide

--- server/books.js
+++ server/books.js
@@ -1,13 +1,8 @@
-const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 const uuidv4 = require('uuid/v4');
 require('dotenv').config();
 
-const apiKey = process.env.GOOGLE_BOOKS_API_KEY
-console.log("Google API Key: " + apiKey)
-
-let searchList = []
 let myBookLists = { 
     toRead: [
         {
@@ -35,25 +30,6 @@ let myBookLists = {
     ]
 } 
 
-// GET books from GoogleBooks endpoint
-router.get('/search/:search', (req, res) => {
-    //fetch API with query params
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + req.params.search + "&key=" + apiKey)
-        .then(response => {
-            
-            searchList = response.data.items.map((book)=>{ return (
-                {
-                    title: book.volumeInfo.title,
-                    authors: book.volumeInfo.authors,
-                    id: book.id
-                })
-            })
-            //grab data for each book and save
-            // console.log(response.data.items)
-        }).then(()=> {res.status(200).send(searchList)}, 
-        error => console.log('An error occurred.', error))
-});
-
 // GET books for MyLists
 router.get('/myBookLists', (req, res) => {
     res.status(200).send(myBookLists)
@@ -70,11 +46,6 @@ router.post('/myBookLists/:bookId', (req, res) => {
 router.put('/myBookLists/:bookId', (req, res) => {
     const bookId = req.params.bookId;
     const bookItem = req.body; // body will require fields for currentLocation and markType (mark as ToRead/HaveRead)
-});
-
-// Delete book from search list
-router.delete('/search/:bookId', function (req, res) {
-    const bookId = req.params.bookId;
 });
 
 // Delete book from Have Read list
