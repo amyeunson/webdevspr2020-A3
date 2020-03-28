8ea62ef6cf3d62d40686c973d37cfd2acb4dfafe
Leah Mamaril <8636027+Lmamaril@users.noreply.github.com>
2020-03-28 09:19:16 -0700

    Middleware added

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const app = express();
 var cors = require('cors');
 
 app.use(cors());
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/books', books);
