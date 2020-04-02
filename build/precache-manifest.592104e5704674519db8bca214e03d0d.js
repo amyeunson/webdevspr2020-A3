3b3d01efdfaa0f85eaf4a9e31f08a8fc1d1b52da
aaeunson <eunson.a@husky.neu.edu>
2020-04-02 13:00:00 -0700

    add build file and updated package

---
+++ build/precache-manifest.592104e5704674519db8bca214e03d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666df329a8b781653e49925aa6edbac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f11dcb19cdf7bc2bfeb",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c23468411b38ecdd7c0a",
+    "url": "/static/css/main.07f8e871.chunk.css"
+  },
+  {
+    "revision": "6f11dcb19cdf7bc2bfeb",
+    "url": "/static/js/2.4cb9ec05.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.4cb9ec05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23468411b38ecdd7c0a",
+    "url": "/static/js/main.1cdf50c7.chunk.js"
+  },
+  {
+    "revision": "15b010c0316d7d80e682",
+    "url": "/static/js/runtime-main.9b30edd0.js"
+  }
+]);
